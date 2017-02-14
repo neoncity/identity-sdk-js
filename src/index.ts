6f9bbba3b65a07a702096385348bc3a3fb0e7d01
Horia Coman <horia141@gmail.com>
2017-02-14 22:57:34 +0200

    First structures.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-import './second'
+import * as m from '@neoncity/common-js/marshall'
+import { MarshalWith} from '@neoncity/common-js/marshall'
+
+
+export enum Role {
+    Regular,
+    Admin
+}
+
+
+export class User {
+    @MarshalWith(m.IdMarshaller)
+    id: number;
+
+    @MarshalWith(m.TimeMarshaller)
+    timeCreated: Date;
+
+    @MarshalWith(m.TimeMarshaller)
+    timeLastUpdated: Date;
+
+    // @MarshalWith(RoleMarshaller)
+    role: Role;
+
+    // @MarshalWith(Auth0UserIdHashMarshaller)
+    auth0UserIdHash: string;
+
+    isAdmin(): boolean {
+        return this.role == Role.Admin;
+    }
+}
