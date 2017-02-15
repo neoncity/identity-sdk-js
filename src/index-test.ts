c0f77444731c47a255eb7804bc75582da11bc1ca
Horia Coman <horia141@gmail.com>
2017-02-15 18:27:50 +0200

    Added User & accompanying stuff.

---
+++ src/index-test.ts
@@ -0,0 +1,74 @@
+import { expect } from 'chai'
+import 'mocha'
+
+import { Auth0UserIdHashMarshaller, Role, User } from './index'
+
+
+describe('User', () => {
+    const UserTestCases = [
+	{
+	    user: new User(1, new Date(Date.UTC(2017, 1, 17)), new Date(Date.UTC(2017, 1, 17)), Role.Admin, '', ''),
+	    isAdmin: true
+	},
+	{
+	    user: new User(1, new Date(Date.UTC(2017, 1, 17)), new Date(Date.UTC(2017, 1, 17)), Role.Regular, '', ''),
+	    isAdmin: false
+	}
+    ];
+    
+    describe('isAdmin', () => {
+	for (let tc of UserTestCases) {
+	    it(`should properly identity admin for ${JSON.stringify(tc.user)}`, () => {
+		expect(tc.user.isAdmin()).to.eql(tc.isAdmin);
+	    });
+	}
+    });
+});
+
+
+describe('Auth0UserIdHashMarshaller', () => {
+    const Hashes = [
+	'0000000000000000000000000000000000000000000000000000000000000000',
+	'0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+    ];
+
+    const BadLengthHashes = [
+	'',
+	'0',
+	'000000000000000000000000000000000000000000000000000000000000000',
+	'00000000000000000000000000000000000000000000000000000000000000000'
+    ];
+
+    const BadContentHashes = [
+	' 000000000000000000000000000000000000000000000000000000000000000',
+	'0123456789Abcdef0123456789Abcdef0123456789Abcdef0123456789Abcdef',
+	'                                                                '
+    ];
+
+    describe('extract', () => {
+	for(let hash of Hashes) {
+	    it(`should parse "${hash}"`, () => {
+		const hashMarshaller = new Auth0UserIdHashMarshaller();
+
+		expect(hashMarshaller.extract(hash)).to.eql(hash);
+	    });
+	}
+
+	for(let hash of BadLengthHashes) {
+	    it(`should throw for bad-length "${hash}"`, () => {
+		const hashMarshaller = new Auth0UserIdHashMarshaller();
+
+		expect(() => hashMarshaller.extract(hash)).to.throw('Expected string to be 64 characters');
+	    });
+	}
+	
+	for(let hash of BadContentHashes) {
+	    it(`should throw for bad-content "${hash}"`, () => {
+		const hashMarshaller = new Auth0UserIdHashMarshaller();
+
+		expect(() => hashMarshaller.extract(hash)).to.throw('Expected all hex characters');
+	    });
+	}
+    });
+});
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import * as m from '@neoncity/common-js/marshall'
-import { MarshalWith} from '@neoncity/common-js/marshall'
+import { MarshalWith } from '@neoncity/common-js/marshall'
 
 
 export enum Role {
-    Regular,
-    Admin
+    Unknown = 0,
+    Regular = 1,
+    Admin = 2
 }
+
+
+export class Auth0UserIdHashMarshaller extends m.StringMarshaller {
+    private static readonly _hexRegExp: RegExp = new RegExp('^[0-9a-f]{64}$');
+
+    filter(s: string) {
+        if (s.length != 64) {
+	    throw new m.ExtractError('Expected string to be 64 characters');
+	}
+
+	if (!Auth0UserIdHashMarshaller._hexRegExp.test(s)) {
+	    throw new m.ExtractError('Expected all hex characters');
+	}
+
+	return s;
+    }
+}
+
 
 
 export class User {
@@ -18,11 +37,23 @@ export class User {
     @MarshalWith(m.TimeMarshaller)
     timeLastUpdated: Date;
 
-    // @MarshalWith(RoleMarshaller)
+    @MarshalWith(m.MarshalEnum(Role))
     role: Role;
 
-    // @MarshalWith(Auth0UserIdHashMarshaller)
+    @MarshalWith(Auth0UserIdHashMarshaller)
     auth0UserIdHash: string;
+
+    @MarshalWith(m.UriMarshaller)
+    pictureUri: string;
+
+    constructor(id: number, timeCreated: Date, timeLastUpdated: Date, role: Role, auth0UserIdHash: string, pictureUri: string) {
+	this.id = id;
+	this.timeCreated = timeCreated;
+	this.timeLastUpdated = timeLastUpdated;
+	this.role = role;
+	this.auth0UserIdHash = auth0UserIdHash;
+	this.pictureUri = pictureUri;
+    }
 
     isAdmin(): boolean {
         return this.role == Role.Admin;
