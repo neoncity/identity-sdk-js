d7c9dfd27a4480a13906efae81ac9cc3290c54a2
Horia Coman <horia141@gmail.com>
2017-04-16 19:43:16 +0300

    Allow - in access tokens as well

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ export class User {
 
 
 export class Auth0AccessTokenMarshaller extends r.StringMarshaller {
-    private static readonly _alnumRegExp: RegExp = new RegExp('^[0-9a-zA-Z_]+$');
+    private static readonly _alnumRegExp: RegExp = new RegExp('^[0-9a-zA-Z_-]+$');
     
     filter(s: string): string {
         if (s.length == 0) {
