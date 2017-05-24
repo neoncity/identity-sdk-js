269296b86b17b64be20baa827029a4f81e2f9ec7
Horia Coman <horia141@gmail.com>
2017-05-24 23:02:31 +0300

    Added an AuthorizationCodeMarshaller for auth0

--- src/auth-info.ts
+++ src/auth-info.ts
@@ -19,6 +19,23 @@ export class Auth0AccessTokenMarshaller extends r.StringMarshaller {
 }
 
 
+export class Auth0AuthorizationCodeMarshaller extends r.StringMarshaller {
+    private static readonly _alnumRegExp: RegExp = new RegExp('^[0-9a-zA-Z]+$');
+    
+    filter(s: string): string {
+        if (s.length == 0) {
+            throw new ExtractError('Expected a string to be non-empty');
+        }
+
+        if (!Auth0AccessTokenMarshaller._alnumRegExp.test(s)) {
+            throw new ExtractError('Should only contain alphanumerics');
+        }
+
+        return s;
+    }
+}
+
+
 export class AuthInfo {
     @MarshalWith(Auth0AccessTokenMarshaller)
     auth0AccessToken: string;
